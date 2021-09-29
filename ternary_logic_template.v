8072b49e1da62c878ad055ab6baefa7a14d0addd
rvem <roman.melnikov@serokell.io>
2021-09-29 18:00:45 +0300

    Publish first homework

---
+++ ternary_logic_template.v
@@ -0,0 +1,23 @@
+module ternary_min(a0, a1, b0, b1, out0, out1);
+  input a0, a1, b0, b1;
+  output out0, out1;
+
+endmodule
+
+module ternary_max(a0, a1, b0, b1, out0, out1);
+  input a0, a1, b0, b1;
+  output out0, out1;
+
+endmodule
+
+module ternary_any(a0, a1, b0, b1, out0, out1);
+  input a0, a1, b0, b1;
+  output out0, out1;
+
+endmodule
+
+module ternary_consensus(a0, a1, b0, b1, out0, out1);
+  input a0, a1, b0, b1;
+  output out0, out1;
+
+endmodule
